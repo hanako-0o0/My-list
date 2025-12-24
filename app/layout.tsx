ae75e31e88ebf57d1e485c3500aefc56477295ae
Jenny Yu <jy1229@students.cs.ubc.ca>
2025-12-23 21:33:07 -0800

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "My List",
   description: "アニメ・ドラマの視聴管理ができるWebアプリ",
+  verification: {
+    google: "oUZ1WAD4eHzdFlGBHDszXRsLJ8EMMQGMA5BtY5XujYk",
+  },
 };
 
 export default function RootLayout({
